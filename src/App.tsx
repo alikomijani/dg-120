e640657d1623b7b63bbc51b1450c81691e6c369c
Ali Komijani <a.komijani@wallex.net>
2024-12-27 10:22:29 +0330

    add persistor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import QueryProvider from "./providers/QueryProvider";
 import Routes from "./widgets/routes";
 import { Provider } from "react-redux";
-import store from "./store";
+import store, { persistor } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 export default function App() {
   return (
     <Provider store={store}>
-      <QueryProvider>
-        <Routes />
-      </QueryProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <QueryProvider>
+          <Routes />
+        </QueryProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store.ts
+++ src/store.ts
@@ -1,17 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cart/cart.slice";
 import authReducer from "./features/auth/auth.slice";
+import { combineReducers } from "@reduxjs/toolkit";
 import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  auth: authReducer,
+});
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    auth: authReducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }).concat(logger),
 });
 export default store;
-
+export const persistor = persistStore(store);
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
